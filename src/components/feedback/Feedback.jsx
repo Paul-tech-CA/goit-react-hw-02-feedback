7453c00645adaffbdd557ca96d053f5cb6290694
Pavel Sakun <axelhomkin@gmail.com>
2021-02-25 01:14:58 +0200

    created jsx markup

--- src/components/feedback/Feedback.jsx
+++ src/components/feedback/Feedback.jsx
@@ -7,11 +7,21 @@ class FeedbacksCounter extends Component {
             <div className={style.feedbacksCounter}>
                 <h1 className={style.title}>Please leave feedback</h1>
                 <div className={style.feedbacksChoice}>
-                    <button type='button' className="button"></button>
-                    <button type='button' className="button"></button>
-                    <button type='button' className="button"></button>
+                    <button type='button' className="button">Good</button>
+                    <button type='button' className="button">Neutral</button>
+                    <button type='button' className="button">Bad</button>
+                </div>
+                <div className={style.statistics}>
+                    <h2 className={style.statisticsTitle}>Statistics</h2>
+                    <ul className={style.statisticsList}>
+                        <li className={style.statisticsItem}>Good: </li>
+                        <li className={style.statisticsItem}>Neutral: </li>
+                        <li className={style.statisticsItem}>Bad: </li>
+                    </ul>
                 </div>
             </div>
         )
     }
 }
+
+export default FeedbacksCounter;
